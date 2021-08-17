373a7fd12e4ee54bb8adb433842bce9396f80e2a
我妻善逸 <2848787564@qq.com>
2021-08-17 15:42:51 +0800

    第一次提交

---
+++ toutiao-m/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>黑马头条</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+
+<style lang="less">
+</style>
---
+++ toutiao-m/src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+// 路由表
+const routes = [
+
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
